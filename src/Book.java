0561e6e13adadd694ba3a1cea3939d5210a585ab
nek0722 <220439@astanait.edu.kz>
2023-02-16 12:13:39 +0600

    database exmpl

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Book {
     String bookName;
     int aitushniki;
     String JKBHIB;
     int adg;
     int a;
+    public void DataBaseConnection(){
+        try{
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/endterm","root","390422");
+            Statement statement=connection.createStatement();
+
+
+        } catch(Exception e)  {
+            e.printStackTrace();
+        }
+    }
 }
