b94bf5362131f86a8ac8e5ed54b33609c1403a78
nek0722 <220439@astanait.edu.kz>
2023-02-16 12:31:37 +0600

    database exmpl

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,25 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Book {
     String bookName;
-    int aitushniki;
-    String JKBHIB;
-    int adg;
-    int a;
+
     public void DataBaseConnection(){
         try{
             Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/endterm","root","390422");
             Statement statement=connection.createStatement();
+            ResultSet rs=statement.executeQuery("SELECT id, bookName,jenre, price FROM `endterm`.`flip`");
+            System.out.println(rs);
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                String genre = rs.getString(3);
+                int price = rs.getInt(4);
+                System.out.println(id + " | " + name + " | " + genre + " | " + price);
+            }
+            rs.close();
 
 
         } catch(Exception e)  {
